f5229ecd7b22b357c985d64dc8e829d7861ee849
Oleg Novikov <thefasttracker@gmail.com>
2017-03-12 19:43:06 +0300

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,5 +17,34 @@ export default {
 		}
 		
 		return Array.isArray(todos) ? todos : []
+	}, 
+
+	filterTodos (todos, showCompleted, searchText) {
+		let filteredTodos = todos
+
+		//filter by showCompleted
+		filteredTodos = filteredTodos.filter((todo) => {
+			return !todo.completed || showCompleted
+		})
+
+		//filter by searchText
+		filteredTodos = filteredTodos.filter((todo) => {
+			let text = todo.text.toLowerCase()
+			return searchText.length === 0 || ~text.indexOf(searchText)
+		})
+
+		//sort todos with non-completed first
+		filteredTodos.sort((a, b) => {
+			if (!a.completed && b.completed) {
+				return -1
+			} else if (a.completed && !b.completed) {
+				return 1
+			} else {
+				return 0
+			}
+
+		})
+
+		return filteredTodos 
 	}
 }
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -51,11 +51,12 @@ export default class TodoApp extends Component {
     }
 
     render() {
-    	let {todos} = this.state
+    	let {todos, showCompleted, searchText} = this.state
+        let filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText)
 		return (
 			<div>
 				<TodoSearch onSearch={this.handleSearch}/>
-				<TodoList todos={todos} onToggle={this.handleToggle}/>
+				<TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
 				<AddTodo onAddTodo={this.handleAddTodo}/>
 
 			</div>
