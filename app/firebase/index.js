f3ceb0cef537147a502c3eecd9cbc5207edbe02d
Oleg Novikov <thefasttracker@gmail.com>
2017-04-26 01:12:16 +0300

    move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase'
-import apiKey from 'app/firebase/apiKey'
 
 try {
-	var config = {
-	    apiKey,
-	    authDomain: "reacttodo-d8b7c.firebaseapp.com",
-	    databaseURL: "https://reacttodo-d8b7c.firebaseio.com",
-	    projectId: "reacttodo-d8b7c",
-	    storageBucket: "reacttodo-d8b7c.appspot.com",
-	    messagingSenderId: "191970357173"
-	}
+	 var config = {
+	    apiKey: process.env.API_KEY,
+	    authDomain: process.env.AUTH_DOMAIN,
+	    databaseURL: process.env.DATABASE_URL,
+	    projectId: process.env.PROJECT_ID,
+	    storageBucket: process.env.STORAGE_BUCKET,
+	    messagingSenderId: process.env.MESSAGING_SENDER_ID
+	  };
 
 	firebase.initializeApp(config);
 
